8394fe5ef72341b025421aab535540e1f268a5bf
jerry1022 <jerryhung1022@gmail.com>
2017-02-01 00:27:50 +0800

    Added Skills

---
+++ src/reducers/reducer-skill.js
@@ -0,0 +1,10 @@
+export default function () {
+  return ( 
+    {
+       id: 1,
+       program: ['python', 'GO', 'C', 'NodeJS','JAVA' ],
+       framework: ['spring', 'express.js', 'pylons', 'flask' ],
+       db: ['MySQL', 'MongoDB', 'Redis']
+    }
+  );
+}
