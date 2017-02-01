84ecb59722eda7b70a02c0dd9b891d873f28031b
jerry1022 <jerryhung1022@gmail.com>
2017-02-01 22:16:51 +0800

    Added react-bootstrap

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,18 +1,60 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
+import {Grid, Row, Col} from 'react-bootstrap';
 
 class Profile extends Component {
   render() {
     return (
-      <div>
-	<h2>Name: {this.props.profile.name}</h2>
-	<h2>Birth: {this.props.profile.birthday}</h2>
-	<h2>Gender: {this.props.profile.gender}</h2>
-	<h2>Address: {this.props.profile.address}</h2>
-	<h2>Contact: {this.props.profile.phone}</h2>
-	<h2>Email: {this.props.profile.email}</h2>
-	<h2>Skype: {this.props.profile.skype}</h2>
-      </div>
+      <Grid>
+        <Row className='show-grid'>
+            <Col xs={2} md={2} lg={2}>
+		<h2>Name:</h2>
+            </Col>
+            <Col xs={1} md={1} lg={1}>
+                <h2>{this.props.profile.name}</h2>
+            </Col>
+        </Row>
+        <Row>
+            <Col xs={2} md={2} lg={2}>
+		<h2>Birth:</h2>
+            </Col>
+            <Col xs={1} md={1} lg={1}>
+                <h2>{this.props.profile.birthday}</h2>
+            </Col>
+        </Row>
+        <Row>
+            <Col xs={2} md={2} lg={2}>
+		<h2>Gender:</h2>
+            </Col>
+            <Col xs={1} md={1} lg={1}>
+                <h2>{this.props.profile.gender}</h2>
+            </Col>
+        </Row>
+        <Row>
+            <Col xsHidden md={2} lg={2}>
+		<h2>Address:</h2>
+            </Col>
+            <Col xsHidden md={10} lg={10}>
+                <h2>{this.props.profile.address}</h2>
+            </Col>
+        </Row>
+        <Row>
+            <Col xs={2} md={2} lg={2}>
+		<h2>Email:</h2>
+            </Col>
+            <Col xs={1} md={1} lg={1}>
+                <h2>{this.props.profile.email}</h2>
+            </Col>
+        </Row>
+        <Row>
+            <Col xs={2} md={2} lg={2}>
+		<h2>Skype:</h2>
+            </Col>
+            <Col xs={1} md={1} lg={1}>
+                <h2>{this.props.profile.skype}</h2>
+            </Col>
+        </Row>
+      </Grid>
     );
   }
 }
