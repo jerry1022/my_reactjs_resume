a73789382aa5b1d92831834f17900a23f8cc93b2
jerry1022 <jerryhung1022@gmail.com>
2017-02-01 22:54:59 +0800

    Modified css and logo

--- src/App.js
+++ src/App.js
@@ -11,10 +11,12 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Jerry Resume</h2>
+      <div>
+        <div className="App">
+          <div className="App-header">
+            <img src='images/face.png' className="App-logo" alt="logo" />
+            <h2>Welcome to Jerry Resume</h2>
+          </div>
         </div>
         <Profile />
         <hr />
